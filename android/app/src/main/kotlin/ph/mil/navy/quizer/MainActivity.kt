b3fb9619f795dadffb38088c032f2af568e49086
Conrado916645 <36912212+Conrado916645@users.noreply.github.com>
2020-04-28 11:46:46 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/ph/mil/navy/quizer/MainActivity.kt
@@ -0,0 +1,6 @@
+package ph.mil.navy.quizer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+    );
+  }
+}

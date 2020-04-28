f705736e6cb2632c9404dfcd5137842d631ef034
Conrado916645 <36912212+Conrado916645@users.noreply.github.com>
2020-04-28 11:48:19 +0800

    initial design

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,50 @@ class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 3,
+          child: Center(
+            child: Text(
+              'Question',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: FlatButton(
+              color: Colors.green,
+              textColor: Colors.white,
+              child: Text('True'),
+              onPressed: () {
+                setState(() {
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: FlatButton(
+              color: Colors.red,
+              textColor: Colors.white,
+              child: Text('False'),
+              onPressed: () {
+                setState(() {
+                });
+              },
+            ),
+          ),
+        ),
 
+      ],
     );
   }
 }
